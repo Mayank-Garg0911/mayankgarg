fe549caecc14a17fc3adcc6589415b4d04d4d7a7
Mayank-Garg0911 <beluga1cat@gmail.com>
2026-04-12 18:17:17 +0530

    fixed footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,7 @@ export default function Footer() {
     { id: 'about', label: 'About' },
     { id: 'skills', label: 'Skills' },
     { id: 'projects', label: 'Projects' },
+    {id: 'achievements', label:'Achievements'},
     { id: 'contact', label: 'Contact' }
   ]
 
