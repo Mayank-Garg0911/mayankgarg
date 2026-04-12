0dbef836c7ff39ff9fa39692959a62c8f3bb35de
Mayank-Garg0911 <beluga1cat@gmail.com>
2026-04-12 18:11:58 +0530

    added profile pic and updated projects\skills cards

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,14 +5,14 @@ export default function About() {
     <section id="about" className="section-container">
       <div className="section-label">01 / About</div>
       <div className="grid-2">
-        <div className="section-title">
-          We create interesting<br />data solutions.
+        <div className="about-image-container">
+          <img src="/profile.jpeg" alt="Profile" className="about-image" />
+          <div className="about-image-glow"></div>
         </div>
         <div className="content-text">
           <p style={{marginBottom: '20px'}}>
             I'm a passionate <span className="highlight">Data Scientist and Full-Stack Developer</span> currently pursuing my BTech in Data Science Engineering at <span className="highlight">Dr. BR Ambedkar NIT Jalandhar</span>.
-          </p>
-          <p>
+            <br /> <br />
             With expertise in machine learning, data analysis, and web development, I combine analytical skills with creative problem-solving to build impactful solutions. I'm obsessed with turning data into actionable insights and creating seamless user experiences.
           </p>
         </div>
